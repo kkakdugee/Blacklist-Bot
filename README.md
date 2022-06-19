46cf58b3bcdf217f945d69c55e6ef33ebc56c1ad
Aaron <75061722+kkakdugee@users.noreply.github.com>
2022-06-19 00:03:16 -0500

    funny files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blacklist Discord Bot
+Discord Bot that blacklists certain words
+
+# How To Use
+- Type "bl" then enter the word/phrase you would like to blacklist. (Ex: bl wowowow)
+- Note: this bot will only listen to the owner of said server.
+
+## [Invite Link](https://discordapp.com/api/oauth2/authorize?client_id=987930100400017418&permissions=17179945984&scope=bot)
+
---
+++ index.js
@@ -0,0 +1,74 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const dotenv = require('dotenv'); 
+dotenv.config();
+client.login(process.env.TOKEN);
+
+const serversArray = [];
+const serverOwners = [];
+var blacklisted = [];
+
+
+client.on("message", message => {
+    let checkOID = serverOwners.indexOf(message.author.id);
+    if(checkOID != -1) {
+        doBlacklist(message);
+    }
+})
+
+function doBlacklist(message) {
+    let content = message.content;
+    if(content.toLowerCase().startsWith('blcheck')) {
+        if(blacklisted.length != 0) {
+            message.channel.send('Here are the current blacklisted words: ');
+            message.channel.send(blacklisted);
+        }
+        else {
+            message.channel.send('There are currently no words or phrases that are blacklisted.')
+        }
+    }
+    else if(content.toLowerCase().startsWith('bl')) {
+        var blacklistedPhrase = content.substring(content.indexOf("l") + 2);
+        if(checkMutualServerOwners(message, message.guild.id)) {
+            message.channel.send('Only the Server owner has permitted access to use this bot!');
+            return;
+        }
+        else {
+            blacklisted.push(blacklistedPhrase);
+        }
+    }
+    else {
+        if(blacklisted.includes(content)) {
+            try {message.delete();}
+            catch(e){}
+        }
+    }
+    return;
+}
+
+
+function checkMutualServerOwners(message, serverID) {
+    let indexOfServerID = serversArray.indexOf(serverID);
+    let checkID = message.author.id;
+    let currentServerOwner = serverOwners[indexOfServerID];
+    return checkID != currentServerOwner && serverOwners.indexOf(checkID) != -1;
+}
+
+
+client.on("ready", () => {
+    console.log('good to go');
+    console.log('');
+    client.guilds.cache.forEach(g => {
+        console.log(g.name);
+        serversArray.push(g.id);
+        serverOwners.push(g.ownerID);
+    });
+    client.user.setPresence({
+        activity: {
+            name: 'bl',
+            type: "LISTENING",
+        },
+        status: "online"
+    })
+    .catch((error) => console.error(error));
+});
